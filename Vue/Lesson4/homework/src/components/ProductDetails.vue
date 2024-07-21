21cb927590b31bfa7024a1b058b125a43aa8b0bd
Marina Sudakova <rubbish_bin@inbox.ru>
2024-07-21 21:55:43 +0500

    hw5

---
+++ Vue/Lesson4/homework/src/components/ProductDetails.vue
@@ -0,0 +1,113 @@
+<template>
+    <div :data-id="product.id" class="featured__item">
+      <a class="featured__cart" :href="product.linkBuy">
+        <div class="featured__add">
+          <p>Add to Cart</p>
+        </div>
+      </a>
+      <div class="featured__content">
+        <img :src="product.image" alt="product" />
+        <h3 class="featured__title">{{ product.title }}</h3>
+        <p class="featured__price">{{ formattedPrice }}</p>
+        <p v-if="product.available">Available</p>
+        <p v-else>Out of stock</p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'ProductDetails',
+    props: {
+      product: {
+        type: Object,
+        required: true
+      }
+    },
+    computed: {
+      formattedPrice() {
+        return `$${Number(this.product.price).toFixed(2)}`
+      }
+    }
+  }
+  </script>
+  
+  <style scoped lang="css">
+  .featured__item {
+    display: block;
+    position: relative;
+    border: 1px solid black;
+    transition: border 0.4s, background-color 0.4s;
+  }
+  .featured__item:hover {
+    border: 1px solid #f16d7f;
+  }
+  .featured__item:hover .featured__cart {
+    opacity: 1;
+  }
+  .featured__cart {
+    position: absolute;
+    z-index: 1;
+    background-color: rgba(0, 0, 0, 0.5);
+    opacity: 0;
+    text-align: center;
+    top: 30%;
+    left: 25%;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    user-select: none;
+    transition: opacity 0.2s;
+  }
+  .featured__add {
+    display: flex;
+    fill: white;
+    gap: 11px;
+    border: 2px solid white;
+    justify-content: center;
+    align-items: center;
+    padding: 11px 13px 9px 13px;
+    color: white;
+    font-size: 13px;
+    line-height: normal;
+    font-weight: 700;
+    transition: background-color 0.4s;
+  }
+  .featured__add:hover {
+    background-color: #f16d7f;
+  }
+  .featured__content {
+    display: flex;
+    flex-direction: column;
+    gap: 16px;
+    padding-bottom: 10px;
+  }
+  .featured__content::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    opacity: 0;
+    transition: opacity 0.4s;
+  }
+  .featured__content:hover::before {
+    opacity: 1;
+  }
+  .featured__title {
+    color: black;
+    font-size: 13px;
+    line-height: normal;
+    font-weight: 400;
+    padding-left: 18px;
+  }
+  .featured__price {
+    color: #f16d7f;
+    font-size: 16px;
+    line-height: normal;
+    font-weight: 700;
+    padding-left: 18px;
+    padding-right: 18px;
+  }
+  </style>
---
+++ Vue/Lesson4/homework/src/components/ProductList.vue
@@ -0,0 +1,100 @@
+<template>
+    <section class="featured center">
+      <div class="featured__items">
+        <product-details v-for="product in products" :key="product.id" :product="product" />
+      </div>
+    </section>
+  </template>
+  
+  <script>
+  import ProductDetails from './ProductDetails.vue'
+  
+  export default {
+    name: 'ProductList',
+    components: {
+      ProductDetails
+    },
+    data() {
+      return {
+        products: [
+          {
+            id: 1,
+            linkBuy: '#',
+            image: '/img/product1.png',
+            title: 'beatiful interior',
+            price: '52.00',
+            available: false
+          },
+          {
+            id: 2,
+            linkBuy: '#',
+            image: '/img/product2.png',
+            title: 'beatiful interior',
+            price: '62.00',
+            available: true
+          },
+          {
+            id: 3,
+            linkBuy: '#',
+            image: '/img/product3.png',
+            title: 'beatiful interior',
+            price: '72.00',
+            available: false
+          },
+          {
+            id: 4,
+            linkBuy: '#',
+            image: '/img/product4.png',
+            title: 'beatiful interior',
+            price: '42.00',
+            available: true
+          },
+          {
+            id: 5,
+            linkBuy: '#',
+            image: '/img/product5.png',
+            title: 'beatiful interior',
+            price: '32.00',
+            available: false
+          },
+          {
+            id: 6,
+            linkBuy: '#',
+            image: '/img/product6.png',
+            title: 'beatiful interior',
+            price: '92.00',
+            available: true
+          },
+          {
+            id: 7,
+            linkBuy: '#',
+            image: '/img/product7.png',
+            title: 'beatiful interior',
+            price: '57.00',
+            available: false
+          },
+          {
+            id: 8,
+            linkBuy: '#',
+            image: '/img/product8.png',
+            title: 'beatiful interior',
+            price: '46.00',
+            available: true
+          }
+        ]
+      }
+    }
+  }
+  </script>
+  
+  <style scoped lang="css">
+  .featured {
+    margin-top: 100px;
+  }
+  .featured__items {
+    margin-top: 60px;
+    display: grid;
+    grid-template-columns: repeat(4, 261px);
+    gap: 32px;
+  }
+  </style>
